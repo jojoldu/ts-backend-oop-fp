3290ced540e300cf3795753d107d32ffc30c317e
jojoldu <jojoldu@gmail.com>
2025-03-29 19:55:56 +0900

    간단한 Entity 추가

---
+++ src/payment/Payment.ts
@@ -0,0 +1,41 @@
+// src/payment/payment.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { PaymentStatus } from './PaymentStatus';
+
+@Entity()
+export class Payment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  orderId: number;
+
+  @Column()
+  amount: number;
+
+  @Column({
+    type: 'enum',
+    enum: PaymentStatus,
+    default: PaymentStatus.PENDING,
+  })
+  status: PaymentStatus;
+
+  @Column({ nullable: true })
+  externalTransactionId: string;
+
+  // OOP 로직: 엔티티 내부에서 상태 제어
+  complete(transactionId: string) {
+    if (this.status !== PaymentStatus.PENDING) {
+      throw new Error(`이미 결제 완료/실패 상태입니다.`);
+    }
+    this.status = PaymentStatus.COMPLETED;
+    this.externalTransactionId = transactionId;
+  }
+
+  fail() {
+    if (this.status !== PaymentStatus.PENDING) {
+      throw new Error(`이미 완료/실패 상태는 fail() 불가`);
+    }
+    this.status = PaymentStatus.FAILED;
+  }
+}
